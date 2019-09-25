4b9ac94ffe8fc502ce2efa2cf91979556129b383
NHUY2708 <nguyenngocnhuy.278@gmail.com>
2019-09-25 15:13:22 +0700

    oo

--- chitietmay.php
+++ chitietmay.php
@@ -1,127 +1,115 @@
 <?php
-class KiemTra{
-    public static function inputIn($giatri, $ten){
+class KiemTra {
+    public static function inputIn($giatri, $ten) {
         do {
             $giatri = readline($ten);
-        }
-        while(!is_numeric($giatri));
-		return $giatri;
+        } while (!is_numeric($giatri));
+        return $giatri;
     }
-    public static function chonLoai($giatri){
+    public static function chonLoai($giatri) {
         do {
             $giatri = readline('Vui lòng chọn 1 để nhập chi tiết đơn || 2 để nhập chi tiết phức: ');
-        }
-        while($giatri != 1 && $giatri != 2);
+        } while ($giatri != 1 && $giatri != 2);
         return $giatri;
     }
 }
-abstract class ChiTietMay{
-
+abstract class ChiTietMay {
     protected $maSo;
-    protected function nhapChiTiet(){
+    protected function nhapChiTiet() {
         $this->maSo = readline('Nhập mã số chi tiết: ');
     }
-    protected function xuatChitiet(){
-        echo "Mã số: ".$this->maSo . "\n";
+    protected function xuatChitiet() {
+        echo "Mã số: " . $this->maSo . "\n";
     }
-    protected function getMaSo(){
+    protected function getMaSo() {
         return $this->maSo;
     }
-
     protected abstract function tinhTien();
     protected abstract function tinhKhoiLuong();
-	protected function timKiem() {
+    protected function timKiem() {
         return $maCT = readline('Nhập mã số chi tiết bạn muốn tìm: ');
-	}
-	}
-
-class ChiTietDon extends ChiTietMay{
+    }
+}
+class ChiTietDon extends ChiTietMay {
     private $giaTien;
     private $khoiLuong;
-    public function nhapChiTiet(){
+    public function nhapChiTiet() {
         parent::nhapChiTiet();
-        $this->giaTien = KiemTra::inputIn($this->giaTien,'Nhap gia tien: ');
-        $this->khoiLuong = KiemTra::inputIn($this->giaTien,'Nhap khoi luong: ');
+        $this->giaTien = KiemTra::inputIn($this->giaTien, 'Nhap gia tien: ');
+        $this->khoiLuong = KiemTra::inputIn($this->giaTien, 'Nhap khoi luong: ');
     }
-    public function xuatChiTiet(){
+    public function xuatChiTiet() {
         parent::xuatChitiet();
         echo 'Khoi Luong: ' . $this->khoiLuong . "\n";
         echo 'Gia Tien: ' . $this->giaTien . "\n";
     }
-    public function tinhTien(){
+    public function tinhTien() {
         return $this->giaTien;
     }
-    public function tinhKhoiLuong(){
+    public function tinhKhoiLuong() {
         return $this->khoiLuong;
     }
-	public function timKiem() {
-		if(parent::timKiem() == $this->getMaSo())
-		{
+    public function timKiem() {
+        if (parent::timKiem() == $this->getMaSo()) {
             echo '>>Kết quả tìm kiếm: ' . "\n";
-			$this->xuatChiTiet();
-        }
-        else {
+            $this->xuatChiTiet();
+        } else {
             echo 'Không tìm thấy chi tiết có mã số này ! ';
         }
-	}
+    }
 }
 class ChiTietPhuc extends ChiTietMay {
     public $arrChiTietCon = [];
     private $soLuong;
-    public function nhapChiTiet(){
+    public function nhapChiTiet() {
         parent::nhapChiTiet();
         $this->soLuong = readline('Nhập số lượng chi tiết con: ');
         $chiTiet = [];
-        for( $i=0 ; $i<$this->soLuong ; $i++){
+        for ($i = 0;$i < $this->soLuong;$i++) {
             $loaiCT = 0;
             $loaiCT = KiemTra::chonLoai($loaiCT);
-                if($loaiCT == 1){
-                    $chiTiet = new ChiTietDon();
-                }
-                else {
-                    if($loaiCT == 2) {
-                        $chiTiet = new ChiTietPhuc();
-                    }
+            if ($loaiCT == 1) {
+                $chiTiet = new ChiTietDon();
+            } else {
+                if ($loaiCT == 2) {
+                    $chiTiet = new ChiTietPhuc();
                 }
             }
-            $chiTiet->nhapChiTiet();
-            array_push($this->arrChiTietCon,$chiTiet);
         }
-    public function xuatChitiet()
-    {
+        $chiTiet->nhapChiTiet();
+        array_push($this->arrChiTietCon, $chiTiet);
+    }
+    public function xuatChitiet() {
         parent::xuatChitiet();
         foreach ($this->arrChiTietCon as $ctCon) {
             $ctCon->xuatChitiet();
         }
-    }	
-	public function timKiem(){
-		foreach ($this->arrChiTietCon as $ctCon) {
-            if(parent::timKiem() == $ctCon->maSo){
-				$this->xuatChiTiet();
-				break;
-			}
-			else {
-				echo 'Không tìm thấy chi tiết có mã số này !';
-			}
+    }
+    public function timKiem() {
+        foreach ($this->arrChiTietCon as $ctCon) {
+            if (parent::timKiem() == $ctCon->maSo) {
+                $this->xuatChiTiet();
+                break;
+            } else {
+                echo 'Không tìm thấy chi tiết có mã số này !';
+            }
         }
-	}
-
-    public function tinhTien(){
+    }
+    public function tinhTien() {
         $giaTien = 0;
-        foreach ($this->arrChiTietCon as $ctCon){
-            $giaTien += $ctCon->tinhTien();
+        foreach ($this->arrChiTietCon as $ctCon) {
+            $giaTien+= $ctCon->tinhTien();
         }
         return $giaTien;
     }
-    public function tinhKhoiLuong(){
+    public function tinhKhoiLuong() {
         $khoiLuong = 0;
-        foreach ($this->arrChiTietCon as $ctCon){
-            $khoiLuong += $ctCon->tinhKhoiLuong();
+        foreach ($this->arrChiTietCon as $ctCon) {
+            $khoiLuong+= $ctCon->tinhKhoiLuong();
         }
         return $khoiLuong;
     }
 }
-
 class May {
     public $maSoMay;
     public $dsChiTiet = [];
@@ -129,14 +117,13 @@ class May {
         $this->maSoMay = readline('Nhập mã số máy: ');
         $soLuong = 0;
         $soLuong = readline('Nhập số lượng chi tiết của máy: ');
-        for($i=1 ; $i <= $soLuong ; $i++){
+        for ($i = 1;$i <= $soLuong;$i++) {
             echo '>>>>>Nhập chi tiết số ' . $i . "\n";
             $nhapLoai = 0;
             $nhapLoai = KiemTra::chonLoai($nhapLoai);
-            if($nhapLoai == 1){
+            if ($nhapLoai == 1) {
                 $chiTiet = new ChiTietDon();
-            }
-            else {
+            } else {
                 $chiTiet = new ChiTietPhuc();
             }
             $chiTiet->nhapChiTiet();
@@ -146,22 +133,22 @@ class May {
     function xuatChiTiet() {
         echo '<<<<<<<Xuất máy' . "\n";
         echo 'Mã số máy: ' . $this->maSoMay . "\n";
-        echo 'Danh sách chi tiết trong máy: ' . "\n" ;
+        echo 'Danh sách chi tiết trong máy: ' . "\n";
         foreach ($this->dsChiTiet as $dsCT) {
             $dsCT->xuatChiTiet();
         }
     }
-    function tinhKhoiLuong(){
+    function tinhKhoiLuong() {
         $khoiLuong = 0;
-        foreach ($this->dsChiTiet as $dsCT){
-            $khoiLuong += $dsCT->tinhKhoiLuong();
+        foreach ($this->dsChiTiet as $dsCT) {
+            $khoiLuong+= $dsCT->tinhKhoiLuong();
         }
         return $khoiLuong;
     }
-    function  tinhTien(){
+    function tinhTien() {
         $giaTien = 0;
-        foreach ($this->dsChiTiet as $dsCT){
-            $giaTien += $dsCT->tinhTien();
+        foreach ($this->dsChiTiet as $dsCT) {
+            $giaTien+= $dsCT->tinhTien();
         }
         return $giaTien;
     }
@@ -170,17 +157,17 @@ class Kho {
     public $tenKho;
     public $soLuongMay;
     public $dsMay = [];
-    function nhapKho(){
+    function nhapKho() {
         $this->tenKho = readline('Nhập tên kho: ');
         $this->soLuongMay = readline('Nhập số lượng máy trong kho: ');
-        for( $i=1; $i<=$this->soLuongMay; $i++){
+        for ($i = 1;$i <= $this->soLuongMay;$i++) {
             echo '>>>>>Nhập Máy số ' . $i . "\n";
             $may = new May();
             $may->nhapMay();
             array_push($this->dsMay, $may);
         }
     }
-    function xuatKho(){
+    function xuatKho() {
         echo 'Tên kho: ' . $this->tenKho;
         echo 'Số lượng máy trong kho: ' . $this->soLuongMay;
         echo 'Danh sách máy trong kho: ' . "\n";
@@ -188,84 +175,79 @@ class Kho {
             $may->xuatChiTiet();
         }
     }
-    function tinhTien(){
+    function tinhTien() {
         $giaTien = 0;
-        foreach ($this->dsMay as $may){
-            $giaTien += $may->tinhTien();
+        foreach ($this->dsMay as $may) {
+            $giaTien+= $may->tinhTien();
         }
         return $giaTien;
     }
-    function tinhKhoiLuong(){
+    function tinhKhoiLuong() {
         $khoiLuong = 0;
-        foreach ($this->dsMay as $may){
-            $khoiLuong += $may->tinhKhoiLuong();
+        foreach ($this->dsMay as $may) {
+            $khoiLuong+= $may->tinhKhoiLuong();
         }
         return $khoiLuong;
     }
-	function timKiem(){
-        $mayTim = readline( 'Nhập mã máy bạn muốn tìm: ' );
+    function timKiem() {
+        $mayTim = readline('Nhập mã máy bạn muốn tìm: ');
         $ketqua = false;
-		foreach ($this->dsMay as $may){
-			if($mayTim == $may->maSoMay){
+        foreach ($this->dsMay as $may) {
+            if ($mayTim == $may->maSoMay) {
                 $may->xuatChiTiet();
-                return $ketqua =true;
+                return $ketqua = true;
             }
         }
-        if( !$ketqua ){
+        if (!$ketqua) {
             echo 'Không tìm thấy máy có mã số bạn tìm !' . "\n";
         }
     }
 }
-function main($chiTietDon, $chiTietPhuc, $may, $kho){
-    echo '-------------------------------------------------QUẢN LÝ CHI TIẾT MÁY ------------------------------------------------' . "\n";
+function main($chiTietDon, $chiTietPhuc, $may, $kho) {
+    echo '------------------------QUẢN LÝ CHI TIẾT MÁY -------------' . "\n";
     echo 'Mời bạn chọn chức năng muốn thực hiện: ' . "\n";
-$chucNang = readline('1. Quản lý chi tiết máy || 2. Quản lý máy || 3. Quản lý kho || Nhập phím bất kì để thoát: ');
-
-switch($chucNang){
-case 1:
-    $nhapLoai = 0;
-    $nhapLoai = KiemTra::chonLoai($nhapLoai);
-
-    if($nhapLoai == 1){
-        $chiTietDon->nhapChiTiet();
-        echo '>>>>>>>Xuất chi tiết' . "\n";
-        $chiTietDon->xuatChiTiet();
-        $chiTietDon->timKiem();
+    $chucNang = readline('1. Quản lý chi tiết máy || 2. Quản lý máy || 3. Quản lý kho || Nhập phím bất kì để thoát: ');
+    switch ($chucNang) {
+        case 1:
+            $nhapLoai = 0;
+            $nhapLoai = KiemTra::chonLoai($nhapLoai);
+            if ($nhapLoai == 1) {
+                $chiTietDon->nhapChiTiet();
+                echo '>>>>>>>Xuất chi tiết' . "\n";
+                $chiTietDon->xuatChiTiet();
+                $chiTietDon->timKiem();
+            } else {
+                if ($nhapLoai == 2) {
+                    $chiTietPhuc->nhapChiTiet();
+                    $chiTietPhuc->xuatChitiet();
+                    echo 'Gia Tien Cua Chi Tiet May: ' . $chiTietPhuc->tinhTien() . "\n";
+                    echo 'Khoi Cua Chi Tiet May: ' . $chiTietPhuc->tinhKhoiLuong() . "\n";
+                }
+            }
+            echo '--------------------------------------------------------------------------------------' . "\n";
+            main($chiTietDon, $chiTietPhuc, $may, $kho);
+        case 2:
+            $may->nhapMay();
+            $may->xuatChiTiet();
+            echo 'Gia Tien Cua Chi Tiet May: ' . $may->tinhTien() . "\n";
+            echo 'Khoi Cua Chi Tiet May: ' . $may->tinhKhoiLuong() . "\n";
+            echo '--------------------------------------------------------------------------------------' . "\n";
+            main($chiTietDon, $chiTietPhuc, $may, $kho);
+        case 3:
+            $kho->nhapKho();
+            echo '-------------------------------------THỐNG KÊ KHO-------------------------------------' . "\n";
+            $kho->xuatKho();
+            echo 'Tong tien cac may trong kho: ' . $kho->tinhTien() . "\n";
+            echo 'Tong khoi luong cac may trong kho ' . $kho->tinhKhoiLuong() . "\n";
+            $kho->timKiem();
+            echo '---------------------------------------------------------------------------------------' . "\n";
+            main($chiTietDon, $chiTietPhuc, $may, $kho);
+        default:
+            break;
     }
-    else {
-        if($nhapLoai == 2){
-            $chiTietPhuc->nhapChiTiet();
-            $chiTietPhuc->xuatChitiet();
-            echo 'Gia Tien Cua Chi Tiet May: ' . $chiTietPhuc->tinhTien() . "\n";
-            echo 'Khoi Cua Chi Tiet May: ' . $chiTietPhuc->tinhKhoiLuong() ."\n";
-        }
-    }
-    echo '-----------------------------------------------------------------------------------------------------------------' . "\n";
-    main($chiTietDon, $chiTietPhuc, $may, $kho);
-case 2:
-    $may->nhapMay();
-    $may->xuatChiTiet();
-    echo 'Gia Tien Cua Chi Tiet May: ' . $may->tinhTien() . "\n";
-    echo 'Khoi Cua Chi Tiet May: ' . $may->tinhKhoiLuong() ."\n";
-    echo '-----------------------------------------------------------------------------------------------------------------' . "\n";
-    main($chiTietDon, $chiTietPhuc, $may, $kho);
-case 3:
-$kho->nhapKho();
-echo '-------------------------------------THỐNG KÊ KHO-----------------------------------------' . "\n";
-$kho->xuatKho();
-echo 'Tong tien cac may trong kho: ' . $kho->tinhTien() . "\n";
-echo 'Tong khoi luong cac may trong kho ' . $kho->tinhKhoiLuong() ."\n";
-$kho->timKiem();
-echo '--------------------------------------------------------------------------------------------------------------------' . "\n";
-main($chiTietDon, $chiTietPhuc, $may, $kho);
-default:
-        break;
 }
-}
-
 $chiTietDon = new ChiTietDon();
 $chiTietPhuc = new ChiTietPhuc();
 $may = new May();
 $kho = new Kho();
-
 main($chiTietDon, $chiTietPhuc, $may, $kho);
